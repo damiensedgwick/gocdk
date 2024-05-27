d0db460072fd58624c3fdefcde8e7c8a5c6a48f7
Damien Sedgwick <damien@elsewhen.com>
2024-05-27 17:09:16 +0100

    implement database layer

--- lambda/database/database.go
+++ lambda/database/database.go
@@ -1,12 +1,19 @@
 package database
 
 import (
+	"lambda-func/types"
+
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
+const (
+	TABLE_NAME = "user"
+)
+
 type DynamoDBClient struct {
-	daatabaseStore *dynamodb.DynamoDB
+	databaseStore *dynamodb.DynamoDB
 }
 
 func NewDynamoDBClient() DynamoDBClient {
@@ -14,6 +21,52 @@ func NewDynamoDBClient() DynamoDBClient {
 	db := dynamodb.New(dbSession)
 
 	return DynamoDBClient{
-		daatabaseStore: db,
+		databaseStore: db,
 	}
+}
+
+func (u DynamoDBClient) DoesUserExist(username string) (bool, error) {
+	result, err := u.databaseStore.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Key: map[string]*dynamodb.AttributeValue{
+			"username": {
+				S: aws.String(username),
+			},
+		},
+	})
+
+	if err != nil {
+		// if there is an error, we want to return true to prevent any inserts
+		return true, err
+	}
+
+	if result.Item == nil {
+		// there is no error and no user, allow inserts
+		return false, nil
+	}
+
+	// there is a user and no error, prevent inserts
+	return true, nil
+}
+
+func (u DynamoDBClient) InsertUser(user types.RegisterUser) error {
+	// to insert data into dynamo it has to be assembled into a type it understands
+	item := &dynamodb.PutItemInput{
+		TableName: aws.String(TABLE_NAME),
+		Item: map[string]*dynamodb.AttributeValue{
+			"username": {
+				S: aws.String(user.Username),
+			},
+			"password": {
+				S: aws.String(user.Password),
+			},
+		},
+	}
+
+	_, err := u.databaseStore.PutItem(item)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
